1834e3c9475fddd6c9ab25a6cf71f5a9c87624a9
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-02-14 13:17:34 +0000

    adds save function and sqlrunner

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require_relative("../models/students.rb")
+
+student1 = Student.new({
+  "first_name" => "Barry",
+  "last_name" => "Trotter",
+  "age" => 11,
+  "house" => "gryffindor"
+
+  })
+
+  student2 = Student.new({
+    "first_name" => "Lon",
+    "last_name" => "Measly",
+    "age" => 11,
+    "house" => "gryffindor"
+    })
+
+    student1.save
+    student2.save
